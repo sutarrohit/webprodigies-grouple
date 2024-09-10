2b0e18a4adc1d13fa119599ab5a76f17f44781b9
sutarrohit <rohitsutar082@gmail.com>
2024-09-10 23:42:07 +0530

    feat: add landing pagea header

---
+++ src/constants/icons.ts
@@ -0,0 +1,15 @@
+type IconListProps = {
+    id: string
+    icon: string
+}
+
+export const ICON_LIST: IconListProps[] = [
+    {
+        id: "0",
+        icon: "general",
+    },
+    {
+        id: "1",
+        icon: "announcement",
+    },
+]
