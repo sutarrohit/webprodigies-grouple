0c68de930640c0fd3d8691a34758c1e347f8ca91
sutarrohit <rohitsutar082@gmail.com>
2024-09-14 19:21:47 +0530

    fix: to deploy on vercel

--- src/app/(landing)/_components/dashboard-snippet/index.tsx
+++ src/app/(landing)/_components/dashboard-snippet/index.tsx
@@ -4,7 +4,7 @@ type Props = {}
 
 const DashboardSnippet = (props: Props) => {
     return (
-        <div className="relative py-20">
+        <div className="relative  py-20">
             <div className="w-full h-3/6 absolute rounded-[50%] radial--blur opacity-40 mx-10" />
             <div className="w-full aspect-video relative">
                 <Image
@@ -13,8 +13,8 @@ const DashboardSnippet = (props: Props) => {
                     className="opacity-[0.95]"
                     alt="snippet"
                     sizes="100vw"
-                    fill
                     objectFit="contain"
+                    fill
                 />
             </div>
         </div>
