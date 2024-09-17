0450c1a8e1128339e20d878df1500372dff6e254
sutarrohit <rohitsutar082@gmail.com>
2024-09-17 22:54:13 +0530

    feat: add sign-up page and api

--- src/actions/auth.ts
+++ src/actions/auth.ts
@@ -14,8 +14,8 @@ export const onAuthenticatedUser = async () => {
             },
             select: {
                 id: true,
-                firstName: true,
-                lastName: true,
+                firstname: true,
+                lastname: true,
             },
         })
 
@@ -24,7 +24,7 @@ export const onAuthenticatedUser = async () => {
                 status: 200,
                 id: user.id,
                 image: clerk.imageUrl,
-                userName: `${user.firstName} ${user.lastName}`,
+                userName: `${user.firstname} ${user.lastname}`,
             }
 
         return {
@@ -33,6 +33,93 @@ export const onAuthenticatedUser = async () => {
     } catch (error) {
         return {
             status: 404,
+        }
+    }
+}
+
+export const onSignUpUser = async (data: {
+    firstname: string
+    lastname: string
+    image: string
+    clerkId: string
+}) => {
+    try {
+        const createdUser = await client.user.create({
+            data: {
+                ...data,
+            },
+        })
+
+        if (createdUser) {
+            return {
+                status: 200,
+                message: "User successfully created",
+                id: createdUser.id,
+            }
+        }
+
+        return {
+            status: 400,
+            message: "User could not be created! Try again",
+        }
+    } catch (error) {
+        return {
+            status: 400,
+            message: "Oops! something went wrong. Try again",
+        }
+    }
+}
+
+export const onSignInUser = async (clerkId: string) => {
+    try {
+        const loggedInUser = await client.user.findUnique({
+            where: {
+                clerkId,
+            },
+            select: {
+                id: true,
+                group: {
+                    select: {
+                        id: true,
+                        channel: {
+                            select: {
+                                id: true,
+                            },
+                            take: 1,
+                            orderBy: {
+                                createdAt: "asc",
+                            },
+                        },
+                    },
+                },
+            },
+        })
+
+        if (loggedInUser) {
+            if (loggedInUser.group.length > 0) {
+                return {
+                    status: 207,
+                    id: loggedInUser.id,
+                    groupId: loggedInUser.group[0].id,
+                    channelId: loggedInUser.group[0].channel[0].id,
+                }
+            }
+
+            return {
+                status: 200,
+                message: "User successfully logged in",
+                id: loggedInUser.id,
+            }
+        }
+
+        return {
+            status: 400,
+            message: "User could not be logged in! Try again",
+        }
+    } catch (error) {
+        return {
+            status: 400,
+            message: "Oops! something went wrong. Try again",
         }
     }
 }
--- src/hooks/authentication/index.tsx
+++ src/hooks/authentication/index.tsx
@@ -1,11 +1,15 @@
-import { useSignIn } from "@clerk/nextjs"
+import { SignUpSchema } from "@/components/form/sign-up/schema"
+import { useSignIn, useSignUp } from "@clerk/nextjs"
+import { OAuthStrategy } from "@clerk/types"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useMutation } from "@tanstack/react-query"
 import { useRouter } from "next/navigation"
+import { useState } from "react"
 import { useForm } from "react-hook-form"
 import { toast } from "sonner"
 import { z } from "zod"
-import { SignInSchema } from "./schema"
+import { onSignUpUser } from "../../actions/auth"
+import { SignInSchema } from "../../components/form/sign-in/schema"
 
 export const useAuthSignIn = () => {
     const { isLoaded, setActive, signIn } = useSignIn()
@@ -70,4 +74,153 @@ export const useAuthSignIn = () => {
         register,
         errors,
     }
+}
+
+export const useAuthSignUp = () => {
+    const { setActive, isLoaded, signUp } = useSignUp()
+    const [creating, setCreating] = useState<boolean>(false)
+    const [verifying, setVerifying] = useState<boolean>(false)
+    const [code, setCode] = useState<string>("")
+
+    const {
+        register,
+        formState: { errors },
+        reset,
+        handleSubmit,
+        getValues,
+    } = useForm<z.infer<typeof SignUpSchema>>({
+        resolver: zodResolver(SignUpSchema),
+        mode: "onBlur",
+    })
+
+    const router = useRouter()
+
+    const onGenerateCode = async (email: string, password: string) => {
+        if (!isLoaded)
+            return toast("Error", {
+                description: "Oops! something went wrong",
+            })
+        try {
+            if (email && password) {
+                await signUp.create({
+                    emailAddress: getValues("email"),
+                    password: getValues("password"),
+                })
+
+                await signUp.prepareEmailAddressVerification({
+                    strategy: "email_code",
+                })
+
+                setVerifying(true)
+            } else {
+                return toast("Error", {
+                    description: "No fields must be empty",
+                })
+            }
+        } catch (error) {
+            console.error(JSON.stringify(error, null, 2))
+        }
+    }
+
+    const onInitiateUserRegistration = handleSubmit(async (values) => {
+        if (!isLoaded)
+            return toast("Error", {
+                description: "Oops! something went wrong",
+            })
+
+        try {
+            setCreating(true)
+            const completeSignUp = await signUp.attemptEmailAddressVerification(
+                {
+                    code,
+                },
+            )
+
+            if (completeSignUp.status !== "complete") {
+                return toast("Error", {
+                    description:
+                        "Oops! something went wrong, status in complete",
+                })
+            }
+
+            if (completeSignUp.status === "complete") {
+                if (!signUp.createdUserId) return
+                const user = await onSignUpUser({
+                    firstname: values.firstname,
+                    lastname: values.lastname,
+                    clerkId: signUp.createdUserId,
+                    image: "",
+                })
+
+                reset()
+
+                if (user.status === 200) {
+                    toast("Success", {
+                        description: user.message,
+                    })
+                    await setActive({
+                        session: completeSignUp.createdSessionId,
+                    })
+                    router.push(`/group/create`)
+                }
+                if (user.status !== 200) {
+                    toast("Error", {
+                        description: user.message + "action failed",
+                    })
+                    router.refresh
+                }
+                setCreating(false)
+                setVerifying(false)
+            } else {
+                console.error(JSON.stringify(completeSignUp, null, 2))
+            }
+        } catch (error) {
+            console.error(JSON.stringify(error, null, 2))
+        }
+    })
+
+    return {
+        register,
+        errors,
+        onGenerateCode,
+        onInitiateUserRegistration,
+        verifying,
+        creating,
+        code,
+        setCode,
+        getValues,
+    }
+}
+
+export const useGoogleAuth = () => {
+    const { signIn, isLoaded: LoadedSignIn } = useSignIn()
+    const { signUp, isLoaded: LoadedSignUp } = useSignUp()
+
+    const signInWith = (strategy: OAuthStrategy) => {
+        if (!LoadedSignIn) return
+        try {
+            return signIn.authenticateWithRedirect({
+                strategy,
+                redirectUrl: "/callback",
+                redirectUrlComplete: "/callback/sign-in",
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    const signUpWith = (strategy: OAuthStrategy) => {
+        if (!LoadedSignUp) return
+        try {
+            return signUp.authenticateWithRedirect({
+                strategy,
+                redirectUrl: "/callback",
+                redirectUrlComplete: "/callback/complete",
+            })
+        } catch (error) {
+            console.error(error)
+        }
+    }
+
+    return { signUpWith, signInWith }
 }
